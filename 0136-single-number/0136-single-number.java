fadc1bbe5baee056f520dc925a6baa4b8c794a4e
Janiish <25janishr06@dhaanishcollege.in>
2026-07-17 14:33:23 +0530

    Time: 12 ms (23.55%), Space: 47 MB (28.03%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res=0;
-        for(int i: nums){
-            res=res^i;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : nums) {
+            if (set.contains(num)) {
+                // We found a pair! Throw it away.
+                set.remove(num); 
+            } else {
+                // First time seeing this number. Keep it.
+                set.add(num); 
+            }
         }
-        return res;
+        
+        // The only number left in the set is our single number
+        return set.iterator().next();
     }
 }
